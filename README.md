19290f3d0aeb042a513e29ad22fc7372a72ed288
Steve Stepp <steppsr@gmail.com>
2023-12-08 20:05:53 -0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # macon
-MACON is tool to create a JSON file containing all the NFT data from Mintgarden for a single NFT Collection.
+MACON is Bash script to create a JSON file containing all the NFT data from Mintgarden for a given NFT Collection.
+
+## Usage
+Usage: macon.sh <collection_id>
+
+Example
+```
+bash macon.sh col16edeh6yc8xgmvp35raynjzggth3chvful5p2v4ats7xqxzcadsmqpsn0lv
+```
+
+## Notes
+
+* The collection ID can be gotten from Mintgarden, Spacescan, or Dexie.
+* The length time the script runs will depend on the number of NFTs. 
+
+---
+
+#### This tool is powered by the Mintgarden API!
---
+++ macon.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# 1st paramter is collection_id
+# Make sure the first parameter isn't blank
+if [ -z "$1" ]; then
+	echo "Usage: $0 <collection_id>"
+	exit 1
+fi
+collection_id=$1
+
+# Get a list of the NFTs from the Mintgarden API
+# Encoded_id is the value we know as "NFT ID"
+nft_list=$(curl -s https://api.mintgarden.io/collections/$collection_id/nfts/ids | jq -r '.[] | .encoded_id')
+
+# Start with an empty string and we will loop through ever NFT and add it's JSON to out variable.
+# Emulate a progress bar by just printing a period each time it processes an NFT.
+mg_nfts_json=""
+echo -n "Processing"
+for nid in $nft_list; do
+    nft_data=$(curl -s https://api.mintgarden.io/nfts/$nid)
+    mg_nfts_json="$mg_nfts_json$nft_data,"
+	printf "."
+done
+printf " Done!"
+echo ""
+
+# Do a little mending of the JSON, removing the last unneeded comma and wrap in square brackets so it's valid JSON.
+mg_nfts_json="[${mg_nfts_json%?}]"
+
+# Get the collection from the last NFT. Also replace any spaces with an underscore
+collection_name=$(echo $nft_data | jq -r '.collection.name')
+cname="${collection_name// /_}"
+
+# Write to an output file in the current directory
+# Be sure to replace any spaces in the collection name with an underscore
+echo "$mg_nfts_json" > ./$cname.json
